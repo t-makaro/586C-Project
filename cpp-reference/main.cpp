a4d531ff67bffcdad5f23e2b0e07524bccd3eb82
Steven Bobyn <stevenbobyn@icloud.com>
2024-10-10 13:57:37 -0700

    stop tracking data/, update main to forward over train and test, update util for loading train and test

--- cpp-reference/main.cpp
+++ cpp-reference/main.cpp
@@ -1,23 +1,26 @@
 #include "nn.cpp"
 #include "utility.cpp"
+#include <chrono>
 
 int main() {
   // Util 0: Read Train Data
-  vector<int> labels;
-  labels.reserve(60000);
+  vector<int> trainLabels;
+  trainLabels.reserve(60000);
   std::cout << "Reading train data..." << std::endl;
-  auto csvTrainData = utility::ReadTrainCSV("../data/train.csv", labels);
+  auto csvTrainData = utility::ReadDatasetCSV("../data/train.csv", trainLabels);
   std::cout << "done." << std::endl;
   std::cout << "Training data size: " << csvTrainData.size() << "x"
             << csvTrainData[0].size() << std::endl;
-  std::cout << "Training labels size: " << labels.size() << std::endl;
+  std::cout << "Training labels size: " << trainLabels.size() << std::endl;
 
   // Util 1: Read Test Data
+  vector<int> testLabels;
   std::cout << "Reading test data..." << std::endl;
-  auto csvTestData = utility::ReadTestCSV("../data/test.csv");
+  auto csvTestData = utility::ReadDatasetCSV("../data/test.csv", testLabels);
   std::cout << "done." << std::endl;
   std::cout << "Test data size: " << csvTestData.size() << "x"
             << csvTestData[0].size() << std::endl;
+  std::cout << "Test labels size: " << testLabels.size() << std::endl;
 
   // Util 2: Read Weights and Biases
   auto biases_a1 = utility::ReadBias("../data/biases_a1.csv");
@@ -35,15 +38,49 @@ int main() {
   nn.copyWeights({weights_a1, weights_a2, weights_o});
   nn.copyBiases({biases_a1, biases_a2, biases_o});
 
-  // NN 2: Forward Pass
+  // NN 2: Forward Pass Training Set
 
   std::cout << "Forward pass over training set..." << std::endl;
   int numCorrect = 0;
+
+  // timing
+  auto start = std::chrono::high_resolution_clock::now();
+
   Vector result(10, 0);
-  for (size_t i = 0; i < csvTrainData.size(); i++) {
-    auto input = csvTrainData[i];
-    auto output = nn.forward(input, result);
-    // check correct
+  for (int i = 0; i < csvTrainData.size(); i++) {
+    Vector input = csvTrainData[i];
+    Vector output = nn.forward(input, result);
+
+    int maxIndex = 0;
+    float maxVal = 0;
+    for (int j = 0; j < output.size(); j++) {
+      if (output[j] > maxVal) {
+        maxVal = output[j];
+        maxIndex = j;
+      }
+    }
+    if (maxIndex == trainLabels[i]) {
+      numCorrect++;
+    }
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = end - start;
+
+  std::cout << "done." << std::endl;
+  std::cout << "Elapsed time: " << elapsed.count() << " seconds." << std::endl;
+  std::cout << "Train Accuracy: " << (float)numCorrect / csvTrainData.size()
+            << std::endl;
+
+  // NN 3: Forward Pass Test Set
+  numCorrect = 0;
+  start = std::chrono::high_resolution_clock::now();
+
+  result = Vector(10, 0);
+  for (int i = 0; i < csvTestData.size(); i++) {
+    Vector input = csvTestData[i];
+    Vector output = nn.forward(input, result);
+
     int maxIndex = 0;
     float maxVal = 0;
     for (size_t j = 0; j < output.size(); j++) {
@@ -52,14 +89,17 @@ int main() {
         maxIndex = j;
       }
     }
-    if (maxIndex == labels[i]) {
+    if (maxIndex == testLabels[i]) {
       numCorrect++;
     }
   }
   std::cout << "done." << std::endl;
+  end = std::chrono::high_resolution_clock::now();
+  elapsed = end - start;
+  std::cout << "Elapsed time: " << elapsed.count() << " seconds." << std::endl;
 
-  // print accuracy
-  std::cout << "Accuracy: " << (float)numCorrect / csvTrainData.size()
+  std::cout << "Test Accuracy: " << (float)numCorrect / csvTestData.size()
             << std::endl;
+
   return 0;
 }

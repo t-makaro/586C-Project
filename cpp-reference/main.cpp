9dbea8429239c90a7e4977093ba4e50c6a96d046
Steven Bobyn <stevenbobyn@icloud.com>
2024-10-10 10:45:14 -0700

    forward pass ported to C++

--- cpp-reference/main.cpp
+++ cpp-reference/main.cpp
@@ -1,39 +1,65 @@
+#include "nn.cpp"
 #include "utility.cpp"
 
 int main() {
-    // utility::PrintCSV("../data/test.csv");
+  // Util 0: Read Train Data
+  vector<int> labels;
+  labels.reserve(60000);
+  std::cout << "Reading train data..." << std::endl;
+  auto csvTrainData = utility::ReadTrainCSV("../data/train.csv", labels);
+  std::cout << "done." << std::endl;
+  std::cout << "Training data size: " << csvTrainData.size() << "x"
+            << csvTrainData[0].size() << std::endl;
+  std::cout << "Training labels size: " << labels.size() << std::endl;
 
-    // Util 1: Read Test Data
-    // auto csvData = utility::ReadTestCSV("../data/test.csv");
+  // Util 1: Read Test Data
+  std::cout << "Reading test data..." << std::endl;
+  auto csvTestData = utility::ReadTestCSV("../data/test.csv");
+  std::cout << "done." << std::endl;
+  std::cout << "Test data size: " << csvTestData.size() << "x"
+            << csvTestData[0].size() << std::endl;
 
-    // Util 2: Read Train Data (/w labels)
-    vector<int> labels;
-    labels.reserve(60000);
-    // auto csvTrainData = utility::ReadTrainCSV("../data/train.csv", labels);
+  // Util 2: Read Weights and Biases
+  auto biases_a1 = utility::ReadBias("../data/biases_a1.csv");
+  auto weights_a1 = utility::ReadWeight("../data/weights_a1.csv");
+  auto biases_a2 = utility::ReadBias("../data/biases_a2.csv");
+  auto weights_a2 = utility::ReadWeight("../data/weights_a2.csv");
+  auto biases_o = utility::ReadBias("../data/biases_o.csv");
+  auto weights_o = utility::ReadWeight("../data/weights_o.csv");
 
-    // for (size_t i = 0; i < labels.size(); i++)
-    // {
-    //     std::cout << labels[i] << std::endl;
-    // }
-    
-    // Util 3: Read Bias for a single layer
-    auto biases_a1 = utility::ReadBias("../data/biases_a1.csv");
+  // NN 0: Init Neural Network
+  std::vector<int> layers = {784, 300, 300, 10};
+  NN nn(layers);
 
-    std::cout << "Size of this bias array: " << biases_a1.size() << std::endl; // 300,
+  // NN 1: Copy Weights and Biases
+  nn.copyWeights({weights_a1, weights_a2, weights_o});
+  nn.copyBiases({biases_a1, biases_a2, biases_o});
 
-    for (size_t i = 0; i < biases_a1.size(); i++)
-    {
-        std::cout << biases_a1[i] << std::endl;
+  // NN 2: Forward Pass
+
+  std::cout << "Forward pass over training set..." << std::endl;
+  int numCorrect = 0;
+  Vector result(10, 0);
+  for (size_t i = 0; i < csvTrainData.size(); i++) {
+    auto input = csvTrainData[i];
+    auto output = nn.forward(input, result);
+    // check correct
+    int maxIndex = 0;
+    float maxVal = 0;
+    for (size_t j = 0; j < output.size(); j++) {
+      if (output[j] > maxVal) {
+        maxVal = output[j];
+        maxIndex = j;
+      }
     }
-
-    // Util 4: Read Weights as vector<vector<float>> for each layer
-    auto weights_a1 = utility::ReadWeight("../data/weights_a1.csv");
-    std::cout << weights_a1.size() << std::endl;
-    std::cout << weights_a1[0].size() << std::endl; // 300, 784
-
-    for (size_t i = 0; i < weights_a1[0].size(); i++)
-    {
-        std::cout << weights_a1[0][i] << std::endl;
+    if (maxIndex == labels[i]) {
+      numCorrect++;
     }
-    return 0;
+  }
+  std::cout << "done." << std::endl;
+
+  // print accuracy
+  std::cout << "Accuracy: " << (float)numCorrect / csvTrainData.size()
+            << std::endl;
+  return 0;
 }
---
+++ cpp-reference/nn.cpp
@@ -0,0 +1,131 @@
+#include <cassert>
+#include <vector>
+
+typedef std::vector<float> Vector;
+typedef std::vector<Vector> Matrix;
+
+class NN {
+public:
+  NN(std::vector<int> layers);
+  ~NN();
+
+  Vector &forward(const Vector &x, Vector &result);
+  void train(const Matrix trainingData, const int iterations,
+             const int batchSize, float learningRate);
+  float evaluate(const Matrix testData);
+
+  void copyWeights(const std::vector<Matrix> weights);
+  void copyBiases(const std::vector<Vector> biases);
+
+private:
+  int numLayers;
+  std::vector<int> layers;
+  std::vector<Matrix> weights;
+  std::vector<Vector> biases;
+  // preallocate memory for forward pass
+  std::vector<Vector> activations;
+  // preallocate memory for backward pass
+  std::vector<Matrix> dWeights;
+  std::vector<Vector> dBiases;
+
+  static Vector &forwardLayer(const Matrix &w, const Vector &b, const Vector &a,
+                              Vector &result);
+  void updateFromBatch(const Matrix batch, const float learningRate);
+  static Vector &multiply(const Matrix &w, const Vector &x, Vector &result);
+  static Vector &add(const Vector &x, const Vector &b, Vector &result);
+  static Vector &sigmoid(Vector &x);
+  static float sigmoid(float x);
+};
+
+NN::NN(std::vector<int> layers) : layers(layers) {
+  numLayers = layers.size();
+  weights.reserve(numLayers - 1);
+  biases.reserve(numLayers - 1);
+
+  activations.reserve(numLayers);
+  for (int i = 0; i < numLayers; i++) {
+    activations.push_back(Vector(layers[i], 0.0));
+    dWeights.push_back(Matrix(layers[i], Vector(layers[i + 1], 0.0)));
+    dBiases.push_back(Vector(layers[i + 1], 0.0));
+  }
+}
+
+NN::~NN() {}
+
+void NN::copyWeights(const std::vector<Matrix> weights) {
+  assert(weights.size() == numLayers - 1);
+  for (int i = 0; i < weights.size(); i++) {
+    assert(weights[i].size() == layers[i + 1]);
+    assert(weights[i][0].size() == layers[i]);
+  }
+  this->weights = weights;
+}
+
+void NN::copyBiases(const std::vector<Vector> biases) {
+  assert(biases.size() == numLayers - 1);
+  for (int i = 0; i < biases.size(); i++) {
+    assert(biases[i].size() == layers[i + 1]);
+  }
+  this->biases = biases;
+}
+
+Vector &NN::forward(const Vector &x, Vector &result) {
+  activations[0] = x;
+  for (int i = 1; i < numLayers; i++) {
+    forwardLayer(weights[i - 1], biases[i - 1], activations[i - 1],
+                 activations[i]);
+  }
+  result = activations[numLayers - 1];
+  return result;
+}
+
+void NN::train(const Matrix trainingData, const int iterations,
+               const int batchSize, float learningRate) {
+  // TODO
+}
+
+void NN::updateFromBatch(const Matrix batch, const float learningRate) {
+  // TODO
+}
+
+float NN::evaluate(const Matrix testData) {
+  // TODO
+  return 0.0;
+}
+
+Vector &NN::forwardLayer(const Matrix &w, const Vector &b, const Vector &a,
+                         Vector &result) {
+  return sigmoid(add(multiply(w, a, result), b, result));
+}
+
+Vector &NN::sigmoid(Vector &x) {
+  for (int i = 0; i < x.size(); i++) {
+    x[i] = sigmoid(x[i]);
+  }
+  return x;
+}
+
+float NN::sigmoid(float x) { return 1.0 / (1.0 + exp(-x)); }
+
+Vector &NN::multiply(const Matrix &w, const Vector &x, Vector &result) {
+  assert(result.size() == w.size() && w[0].size() == x.size());
+
+  for (int i = 0; i < w.size(); i++) {
+    float sum = 0;
+    for (int j = 0; j < w[i].size(); j++) {
+      sum += w[i][j] * x[j];
+    }
+    result[i] = sum;
+  }
+
+  return result;
+}
+
+Vector &NN::add(const Vector &x, const Vector &b, Vector &result) {
+  assert(x.size() == b.size() && x.size() == result.size());
+
+  for (int i = 0; i < x.size(); i++) {
+    result[i] = x[i] + b[i];
+  }
+  return result;
+}
